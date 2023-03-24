a0e51715b74cb3a2eaa37e9108019a2d989c4d38
Wes <signerwesley@gmail.com>
2023-03-24 15:55:06 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public class Area
+{
+    public static void Main()
+    {
+        Console.WriteLine("Please, enter the Length of your rectangle: ");
+        int Length = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Thank you. Now, please enter the width of your rectangle: ");
+        int Width= Convert.ToInt32(Console.ReadLine());
+        int Area = Length * Width;
+        Console.WriteLine("A rectangle with a length of " + Length +" and a width of " + Width + " is " +Area);
+        Console.ReadKey();
+    }
+    }
